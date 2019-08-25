084992828212701085a9f7465054b483dbefb4a1
Blake <howe.bobby@gmail.com>
2019-08-24 21:30:37 -0500

    cleanup

--- controller.php
+++ controller.php
@@ -1,7 +1,20 @@
 <?php
 
+require_once('config.php');
+
 require __DIR__ . '/vendor/autoload.php';
 use Twilio\Rest\Client;
 use Twilio\Exceptions\TwilioException;
 
 
+$client = new Client($sid, $token);
+
+
+$client->messages->create(
+    // Where to send a text message (your cell phone?)
+    '6015001794',
+    array(
+        'from' => $from,
+        'body' => 'I sent this message in under 10 minutes!'
+    )
+);
