166d7b8fbdbdc35e2ea4502f0df4011d3c152f3c
Blake <howe.bobby@gmail.com>
2019-09-02 13:14:54 -0500

    webhook test

--- webhook.php
+++ webhook.php
@@ -9,3 +9,11 @@ function execPrint($command) {
 
 // Print the exec output inside of a pre element
 print("<pre>" . execPrint("git pull") . "</pre>");
+
+log($_POST);
+
+function log_to_file($thearray){
+
+    file_put_contents("webook.log", print_r($_POST, true),FILE_APPEND);
+  
+  }
