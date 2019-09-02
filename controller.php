178de9140509e9279633f6469785e351a0f16b4e
Blake <howe.bobby@gmail.com>
2019-09-02 12:07:45 -0500

    relays SMS to phone

--- controller.php
+++ controller.php
@@ -12,10 +12,14 @@
 require_once('config.php');
 require_once('functions.php');
 
+//var_dump($_POST);
+
 if ($debug){
   log_to_file($_POST);
   die;
 }
+
+
 
 require __DIR__ . '/vendor/autoload.php';
 use Twilio\Rest\Client;
@@ -26,10 +30,37 @@ $workit = $_POST;
 //determines what post type is coming from the twillo rest api
 if (post_type_sms($workit)){    
     save_sms_log($workit);
+    //forard the text
+    if ($_POST['From'] !== $forwarded_to){ //if its not a message from here kick it over there
+
+      try {
+        $messageid =   $client->messages->create(
+            $forwarded_to,
+           /* array(
+                'from' => $twillo_number,
+                'body' => "dsfsdfds"
+            )*/
+
+            $_POST
+        );
+
+       //echo $messageid;
+    }
+    catch (Exception $e) {
+        $TwilioError = "Error: " . $e->getMessage();
+        echo $TwilioError;
+    }
+
+    }
+    else{ //if its a message from me figure out who it needs to go to.
+
+
+    }
+    
+
 }else{
   
     save_phone_log($workit);
 }
-
 
 
