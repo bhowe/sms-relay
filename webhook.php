675b80c8e9688e3a79aa2787d0a4824754481ab8
Blake <howe.bobby@gmail.com>
2019-09-02 13:19:40 -0500

    webhook test

--- webhook.php
+++ webhook.php
@@ -8,7 +8,7 @@ function execPrint($command) {
 }
 
 // Print the exec output inside of a pre element
-print("<pre>" . execPrint("git fetch --all") . "</pre>");
+print("<pre>" . execPrint("git pull --no-edit") . "</pre>");
 
 log($_POST);
 
