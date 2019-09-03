1e5a4cf9e063edfad6fac15e17236284aac20726
Blake <howe.bobby@gmail.com>
2019-09-02 20:40:31 -0500

    webhook test

--- webhook.php
+++ webhook.php
@@ -1,9 +1,5 @@
 <?php
-
-echo 'test';
-
 include("config.php");
- //rest
 function execPrint($command) {
   $result = array();
   exec($command, $result);
@@ -11,6 +7,5 @@ function execPrint($command) {
       print($line . "\n");
   }
 }
-//dsfdv
 // Print the exec output inside of a pre element
 print("<pre>" . execPrint($git_pull_command) . "</pre>");
